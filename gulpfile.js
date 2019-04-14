15bb4528bdd12448b76d913463722906dd37b905
codemyeyes <codemyeyes@gmail.com>
2019-04-14 21:25:38 +0700

    init project

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+
+/*
+    -- TOP LEVEL FUNCTIONS --
+    gulp.task - Define tasks
+    gulp.src - Point tofiles to use
+    gulp.dest - Point to folder to output
+    gulp.watch - Watch files and folders for changes
+*/
+
+// Log Message
+gulp.task('msg', function () {
+    return new Promise(function(resolve, reject) {
+        console.log('Gulp is running......');
+        resolve();
+    });
+});
+
+// Copy All HTML files
+gulp.task('copyHtml', function () {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+// Optimize Images
+gulp.task('imageMin', function () {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+// Minify JS
+gulp.task('minify', function () {
+    return gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Compile Sass
+gulp.task('sass', function () {
+    return gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// gulp.task('default', ['msg','copyHtml','imageMin','minify','sass']);
+gulp.task('default', gulp.parallel('msg','copyHtml','imageMin','minify','sass'));
